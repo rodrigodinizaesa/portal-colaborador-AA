50201dec008b87e657e628aaf173383e85fcec55
rodrigodinizaesa <27932@aesa.edu.pt>
2026-06-26 21:08:57 +0100

    Delete topbar.php

--- topbar.php
+++
@@ -1,9 +0,0 @@
-<div class="page-header">
-  <button class="btn-primary">
-    <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" aria-hidden="true">
-      <line x1="12" y1="5" x2="12" y2="19"/>
-      <line x1="5" y1="12" x2="19" y2="12"/>
-    </svg>
-    Novo Pedido
-  </button>
-</div>
